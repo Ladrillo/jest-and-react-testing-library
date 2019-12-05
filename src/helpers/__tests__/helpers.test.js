8f67d10eac6c7dde9d972daf12c4974fd7c9789b
Gabriel Cabrejas <gabriel@cabrejas.net>
2019-12-05 09:39:47 +0100

    wip

--- src/helpers/__tests__/helpers.test.js
+++ src/helpers/__tests__/helpers.test.js
@@ -1,5 +1,12 @@
-import { operations } from '../helpers'
-
 describe('helpers', () => {
-
+  it('jest is working', () => {
+    expect(5).toBeDefined()
+    expect(5).toBeTruthy()
+    expect(5).toBeGreaterThan(4)
+    expect(5).not.toBeFalsy()
+    expect(5).toBeLessThanOrEqual(5)
+    expect(5).not.toBeNaN()
+    expect('5').toMatch(/5/)
+    expect(() => { throw new Error('That sounds like trolling') }).toThrow()
+  })
 })
