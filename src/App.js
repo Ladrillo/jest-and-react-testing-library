b49244b628f0596b6c76121bcf3b2dcc09652ce3
Gabriel Cabrejas <gabriel@cabrejas.net>
2019-12-05 11:56:59 +0100

    wip

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+function App({ insult }) {
   return (
     <div className="App lady gaga">
       <header className="App-header">
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React, you {insult}
         </a>
       </header>
     </div>
