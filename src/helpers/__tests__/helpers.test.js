d0fc8e581ae344f735756fd5688a5deaa1f4221f
Gabriel Cabrejas <gabriel@cabrejas.net>
2019-12-05 11:09:39 +0100

    wip

--- src/helpers/__tests__/helpers.test.js
+++ src/helpers/__tests__/helpers.test.js
@@ -40,16 +40,16 @@ describe('helpers', () => { // describe block
     })
 
     it('calculates the right sum', () => {
-
+      expect(operations(2, 3).sum).toBe(5)
     })
     it('calculates the right product', () => {
-      
+      expect(operations(2, 3).product).toBe(6)
     })
     it('calculates the right max', () => {
-      
+      expect(operations(2, 3).max).toBe(3)
     })
     it('calculates the right numbers', () => {
-      
+      expect(operations(2, 3).numbers).toEqual([2, 3])
     })
   })
 })
