232bd25a2e68e9e01da9fa9559d2b6b972c36d9e
Gabriel Cabrejas <gabriel@cabrejas.net>
2019-12-05 11:55:58 +0100

    wip

--- src/App.test.js
+++ src/App.test.js
@@ -11,5 +11,11 @@ afterEach(rtl.cleanup)
 it('renders without crashing', () => {
   // step 3 - mount the component we want to test
   const wrapper = rtl.render(<App />)
-  wrapper.debug()
+  // wrapper.debug()
+  // step 4 - capture the element of interest BY "READABLE" PROPERTY
+  // we normally care about the readable text
+  // or the aria attributes (screen readers)
+  const learnReactElement = wrapper.queryByText(/learn react/i)
+  // step 5 - assert that the element is there
+  expect(learnReactElement).toBeInTheDocument()
 });
