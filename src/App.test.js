5d2b74a78894906a3ce32cf4a2748d3a3c45321c
Gabriel Cabrejas <gabriel@cabrejas.net>
2019-12-05 11:43:47 +0100

    wip

--- src/App.test.js
+++ src/App.test.js
@@ -1,5 +1,6 @@
 import React from 'react';
-
+import * as rtl from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect'
 import App from './App';
 
 it('renders without crashing', () => {
